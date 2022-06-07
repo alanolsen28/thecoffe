e2f14e944f30965749772d555d918ff39f7fd24c
alanolsen28 <103078167+alanolsen28@users.noreply.github.com>
2022-06-07 19:06:56 -0300

    buenas

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-function suma(num1, num2) {
-    let total = num1 + num2;
-    return totalFinal
-}
-
 function multiplicar(num1, num2) {
     let total = num1 * num2;
     return total
@@ -28,9 +23,13 @@ const cafe = 400;
 
 let cantidadCafe = parseInt(prompt(usuario + " ¿Cuantos Cafes Queres?"));
 
+while (cantidadCafe <= 0) {
+    cantidadCafe = parseInt(prompt(usuario + " ¿Cuantos Cafes Queres?"))
+};
+
 let total = multiplicar(cantidadCafe, cafe);
 
-alert("El total es "+ total)
+alert("El total es " + total)
 
 // extra
 
@@ -40,18 +39,36 @@ const cookie = 100
 
 let extra = prompt(usuario + " ¿Quieres sumarle un extra? Hay torta, budin y cookie?");
 
-if (extra === "no") {
-    alert("El total es " + total)    
-}
-else if (extra === "torta") {
-    alert("el total es " + (total + torta))
-}
-else if (extra === "budin") {
-    alert("el total es " + (total + budin))
-}
-else if (extra === "cookie") {
-    alert("el total es " + (total + cookie))
-}
-else {alert("El total es " + total)}
 
-alert ("Gracias por comprar con nosotros")
+if (extra === "no") {
+    alert("El total es " + total)
+} else if (extra === "torta") {
+    alert("el total es " + (total + torta))
+} else if (extra === "budin") {
+    alert("el total es " + (total + budin))
+} else if (extra === "cookie") {
+    alert("el total es " + (total + cookie))
+} else {
+    alert("El total es " + total)
+}
+
+
+
+alert("Gracias por comprar con nosotros")
+
+
+
+class persona {
+    constructor (nombre, años, altura, pais) {
+
+        this.nombre = nombre;
+        this.años = años;
+        this.altura = altura;
+        this.pais = pais
+    }
+    gerAños() {
+        return this.años
+    }
+}
+const homero = new persona("homero", 40, 150, "argentina")
+
