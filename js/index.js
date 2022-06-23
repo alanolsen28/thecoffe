267b00fef83dc3f85b052e624a67e411639b31aa
alanolsen28 <103078167+alanolsen28@users.noreply.github.com>
2022-06-23 18:03:30 -0300

    eventos

--- js/index.js
+++ js/index.js
@@ -1,27 +1,77 @@
-
-
 const cont = document.querySelector("#cont");
 
-for (let p of productos) {
+for (let prod of productos) {
   cont.innerHTML += `
       <div class="col-sm-3">
           <div class="card bg-dark text-light m-1">
-          <img class="card-img-top" src="${p.img}" alt="">
+          <img class="card-img-top" src="${prod.img}" alt="">
           <div class="card-body">
-          <h5 class="card-title">${p.nombre}</h5>
-           <p class="card-text">$ ${p.precio}</p>
-           <button onclick="agregar()" class="btn btn-lg shadow destacado agregarCarrito" type="submit"
-           style="background-color:#998774;">COMPRAR</button>
+          <h5 class="card-title">${prod.nombre}</h5>
+           <p class="card-text">$ ${prod.precio}</p>
+           <a class="btn shadow destacado aCarrito" id=${prod.nombre}>COMPRAR</a>
+      </div>
+      </div>
       </div>
   `
-
 }
 
-let cart = []
+
+//CARRITO
+
+const cart = []
+
+const aCarrito = document.getElementsByClassName(`aCarrito`)
+
+
+for (let p of aCarrito) {
+
+  p.addEventListener('click', (e) => {
+
+    const carritoid = e.target.id;
+
+    findAndAddProd(carritoid);
+
+    renderCart();
+
+  });
+}
+
+
+function findAndAddProd(id) {
+
+const produ = productos.find ( produ => {
+  return produ.nombre === id;
+})
+
+cart.push(produ)
+
 console.log(cart);
 
-for (let p of productos) {
-  agregar = () => { cart.push(p) 
-  alert("Agregaste " + p.nombre + "$" + p.precio + " al carrito");}
 }
 
+const detalleDeCompra = document.getElementById('detalleDeCompra')
+
+function renderCart() {
+
+  detalleDeCompra.innerHTML = '';
+
+  for (let item of cart) {
+
+  const cardCarrito = document.createElement('div');
+
+  cardCarrito.classList.add('col-12');
+
+  cardCarrito.innerHTML = `
+  <div class="col-sm-3">
+      <div class="card bg-dark text-light m-1" style="width: 5rem;">
+      <img class="card-img-top" src="${item.img}" alt="">
+      <div class="card-body">
+       <p class="card-text">$ ${item.precio}</p>
+  </div>
+  </div>
+  </div>
+  `
+
+  detalleDeCompra.appendChild(cardCarrito)
+
+}}
--- js/productos.js
+++ js/productos.js
@@ -1,73 +1,72 @@
 const productos = [{
 
-    id: 1,
     nombre: "Espresso",
     precio: 500,
     img: "../imagenes/imagenes_menu/1.jpg"
 },
 {
-    id: 2,
+   
     nombre: "Gibraltar",
     precio: 500,
     img: "../imagenes/imagenes_menu/2.jpg"
 },
 {
-    id: 3,
+    
     nombre: "Cappuccino",
     precio: 600,
     img: "../imagenes/imagenes_menu/3.jpg"
 },
 {
-    id: 4,
+    
     nombre: "Latte",
     precio: 600,
     img: "../imagenes/imagenes_menu/4.jpg"
 },
 {
-    id: 5,
+    
     nombre: "Bulletproof",
     precio: 700,
     img: "../imagenes/imagenes_menu/5.jpg"
 },
 {
-    id: 6,
+    
     nombre: "Rumorosa",
     precio: 700,
     img: "../imagenes/imagenes_menu/6.jpg"
 },
 {
-    id: 7,
+    
     nombre: "Special",
     precio: 700,
     img: "../imagenes/imagenes_menu/7.jpg"
 },
 {
-    id: 8,
-    nombre: "Lavanda ",
+    
+    nombre: "Lavanda",
     precio: 600,
     img: "../imagenes/imagenes_menu/8.jpg"
 },
 {
-    id: 9,
+    
     nombre: "Chai",
     precio: 600,
     img: "../imagenes/imagenes_menu/9.jpg"
 },
 {
-    id: 10,
+    
     nombre: "Matcha",
     precio: 700,
     img: "../imagenes/imagenes_menu/10.jpg"
 },
 {
-    id: 11,
+    
     nombre: "Chocolate",
     precio: 600,
     img: "../imagenes/imagenes_menu/11.jpg"
 },
 {
-    id: 12,
-    nombre: "Cold ",
+    
+    nombre: "Cold",
     precio: 600,
     img: "../imagenes/imagenes_menu/12.jpg"
 }
